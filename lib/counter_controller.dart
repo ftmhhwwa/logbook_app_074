ef94a4615d9df904adec6dd6ba4a95534f4bb97e
ftmhhwwa <fatimahhawwa0201@gmail.com>
2026-02-15 15:46:32 +0700

    Task 1: The Multi-Step Counter (Low Order Thinking - LOTS)

--- lib/counter_controller.dart
+++ lib/counter_controller.dart
@@ -1,9 +1,16 @@
 class CounterController {
   int _counter = 0; // Variabel private (Enkapsulasi)
+  int _step = 1; // Variabel private untuk langkah increment/decrement
 
   int get value => _counter; // Getter untuk akses data
+  int get step => _step; // Getter untuk akses langkah
 
-  void increment() => _counter++;
-  void decrement() { if (_counter > 0) _counter--; }
+  void setStep(int newValue) {
+    _step = newValue; // Setter untuk mengubah langkah increment/decrement
+  }
+
+  void increment() => _counter += _step;
+  void decrement() { if (_counter > 0) _counter -= _step; else _counter = 0; }
+  
   void reset() => _counter = 0;
 }
--- lib/counter_view.dart
+++ lib/counter_view.dart
@@ -13,19 +13,45 @@ class _CounterViewState extends State<CounterView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("LogBook: Versi SRP")),
+      appBar: AppBar(title: const Text("LogBook: Versi Task 1")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            //Input Step
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: "Masukkan Step"),
+                onChanged: (value) =>
+                    _controller.setStep(int.tryParse(value) ?? 1),
+              ),
+            ),
             const Text("Total Hitungan:"),
             Text('${_controller.value}', style: const TextStyle(fontSize: 40)),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() => _controller.increment()),
-        child: const Icon(Icons.add),
+      //tombol tambah dan kurang
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => setState(() => _controller.decrement()),
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: () => setState(() => _controller.increment()),
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: () => setState(() => _controller.reset()),
+            child: const Icon(Icons.refresh),
+          ),
+        ],
       ),
     );
   }
