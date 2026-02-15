6e3301e5e02283dc6588e1bd3a21b5988096b21e
ftmhhwwa <fatimahhawwa0201@gmail.com>
2026-02-15 15:06:19 +0700

    menerapkan konsep SRP

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'counter_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,9 +29,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+     // home: const MyHomePage(title: 'LogBook: Versi SRP'),
+      home: const CounterView(),
     );
   }
 }
